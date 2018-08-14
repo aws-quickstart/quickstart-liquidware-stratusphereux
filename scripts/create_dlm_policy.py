81f213c4d8cc9aadbbd20250e9f62421baba99e2
Aaron Lima <aarolima@amazon.com>
2018-08-14 11:27:56 -0400

    update create_dlm_policy to run locally instead of lambda

--- scripts/create_dlm_policy.py
+++ scripts/create_dlm_policy.py
@@ -1,18 +1,14 @@
 # This script creates an Amazon Data Lifecycle Management Schedule 
 
 import boto3
+from sys import argv
+
+execution_role, target_tag_key, target_tag_value, schedule_name, snapshot_interval, schedule_time, retention, aws_region = argv
 
 dlm_client = boto3.client('dlm')
+supported_regions = ['us-east-1','us-west-2','eu-west-1']
 
-def lambda_handler(event, context):
-    execution_role = event['ResourceProperties']['ExecutionRole']
-    target_tag_key = event['ResourceProperties']['TargetTags']['Key']
-    target_tag_value = event['ResourceProperties']['TargetTags']['Value']
-    retention = event['ResourceProperties']['Retention']
-    snapshot_interval = event['ResourceProperties']['ScheduleInterval']
-    schedule_time = event['ResourceProperties']['ScheduleTime']
-    schedule_name = event['ResourceProperties']['ScheduleName']
-
+def create_dlm_policy(execution_role, target_tag_key, target_tag_value, schedule_name, snapshot_interval, schedule_time, retention):
     # Creates the Amazon Data Lifecycle Management Schedule for the Workload Specified in the CFN Script
     create_policy = dlm_client.create_lifecycle_policy(
         ExecutionRoleArn = execution_role,
@@ -45,3 +41,8 @@ def lambda_handler(event, context):
             ]
         }
     )
+
+if aws_region in supported_regions:
+    create_dlm_policy(execution_role, target_tag_key, target_tag_value, schedule_name, snapshot_interval, schedule_time, retention)
+else:
+    print("Not in a valid DLM Region")
