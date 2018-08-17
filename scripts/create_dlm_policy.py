0beeb11dc168d1f6c4e940c0b3d067ffcbcb32cd
Aaron Lima <aarolima@amazon.com>
2018-08-17 09:09:35 -0400

    modify stratux template, update create_dlm script to run local

--- scripts/create_dlm_policy.py
+++ scripts/create_dlm_policy.py
@@ -1,12 +1,16 @@
 # This script creates an Amazon Data Lifecycle Management Schedule 
 
+import requests
 import boto3
 from sys import argv
 
-execution_role, target_tag_key, target_tag_value, schedule_name, snapshot_interval, schedule_time, retention, aws_region = argv
+execution_role, target_tag_key, target_tag_value, schedule_name, snapshot_interval, schedule_time, retention = argv
 
 dlm_client = boto3.client('dlm')
+
 supported_regions = ['us-east-1','us-west-2','eu-west-1']
+
+aws_region = requests.get('http://169.254.169.254/latest/dynamic/instance-identity/document').get('region', 'Not Found')
 
 def create_dlm_policy(execution_role, target_tag_key, target_tag_value, schedule_name, snapshot_interval, schedule_time, retention):
     # Creates the Amazon Data Lifecycle Management Schedule for the Workload Specified in the CFN Script
@@ -45,4 +49,4 @@ def create_dlm_policy(execution_role, target_tag_key, target_tag_value, schedule
 if aws_region in supported_regions:
     create_dlm_policy(execution_role, target_tag_key, target_tag_value, schedule_name, snapshot_interval, schedule_time, retention)
 else:
-    print("Not in a valid DLM Region")
+    print("DLM not available in Region")
