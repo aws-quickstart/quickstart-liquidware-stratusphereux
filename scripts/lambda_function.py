a224325779f8fbcc8a398d88315fa180230dbf8e
Aaron Lima <aarolima@amazon.com>
2018-08-22 19:13:02 -0400

    updated dlm policy lambda to include update and delete actions

--- scripts/lambda_function.py
+++ scripts/lambda_function.py
@@ -1,14 +1,13 @@
 # This lambda function creates an Amazon Data Lifecycle Management Schedule 
 # from a Cloudformation Custom Resource
 
-import boto3
-
 def lambda_handler(event, context):
+    import json, boto3
     from botocore.vendored import requests
-    import json
-    
+
     non_supported_regions = ['ap-northeast-3','eu-west-3']
     aws_region = event['ResourceProperties']['AWSRegion']
+    req_type = event['RequestType']
     dlm_client = boto3.client('dlm')
     
     SUCCESS = "SUCCESS"
@@ -49,7 +48,7 @@ def lambda_handler(event, context):
             print("send(..) failed executing requests.put(..): " + str(e))
     
     
-    # Creates the Amazon Data Lifecycle Management Schedule for the Workload Specified in the CFN Script
+    # Creates the Amazon Data Lifecycle Management Schedule for the workload specified in the CFN Script
     def create_dlm_policy(event):
         execution_role = event['ResourceProperties']['ExecutionRole']
         target_tag_key = event['ResourceProperties']['TargetTags']['Key']
@@ -89,12 +88,78 @@ def lambda_handler(event, context):
                 ]
             }
         )
-        send(event, context, SUCCESS, create_policy)
+        print(create_policy)
+        send(event, context, SUCCESS, create_policy, create_policy['PolicyId'])
     
-    if aws_region not in non_supported_regions:
-        try:
-            create_dlm_policy(event)
-        except Exception as exc:
-            send(event, context, FAILED , exc)
-    else:
-        send(event, context, SUCCESS, NONSUPPORTED)
+    # Deletes the Amazon Data Lifecycle Management Schedule for the workload specified in the CFN Script
+    def delete_dlm_policy(event):
+        policy_id = event['PhysicalResourceId']
+
+        delete_policy = dlm_client.delete_lifecycle_policy(
+            PolicyId = event['PhysicalResourceId']
+            )
+        print(delete_policy)
+        send(event, context, SUCCESS, delete_policy)
+
+    # Updates the Amazon Data Lifecycle Management Schedule for the workload specified in the CFN Script
+    def update_dlm_policy(event):
+        policy_id = event['PhysicalResourceId']
+        execution_role = event['ResourceProperties']['ExecutionRole']
+        target_tag_key = event['ResourceProperties']['TargetTags']['Key']
+        target_tag_value = event['ResourceProperties']['TargetTags']['Value']
+        retention = event['ResourceProperties']['Retention']
+        snapshot_interval = event['ResourceProperties']['ScheduleInterval']
+        schedule_time = event['ResourceProperties']['ScheduleTime']
+        policy_state = event['ResourceProperties']['PolicyState']
+
+        update_policy = dlm_client.update_lifecycle_policy(
+            PolicyId= policy_id,
+            ExecutionRoleArn = execution_role,
+            State = policy_state,
+            Description='DLM Schedule Updated for {} by Quick Start'.format(target_tag_value),
+            PolicyDetails = {
+                'ResourceTypes': [
+                    'VOLUME',
+                ],
+                'TargetTags': [
+                    {
+                        'Key': target_tag_key,
+                        'Value': target_tag_value
+                    },
+                ],
+                'Schedules': [
+                    {
+                        'Name': '{} DLM Schedule'.format(target_tag_value),
+                        'CreateRule': {
+                            'Interval': int(snapshot_interval),
+                            'IntervalUnit': 'HOURS',
+                            'Times': [
+                                schedule_time,
+                            ]
+                        },
+                        'RetainRule': {
+                            'Count': int(retention)
+                            }
+                    },
+                ]
+            }
+        )
+        print(update_policy)
+        send(event, context, SUCCESS, update_policy, policy_id)
+        
+    actions = {
+        'Create': create_dlm_policy,
+        'Delete': delete_dlm_policy,
+        'Update': update_dlm_policy
+    }
+
+    try:
+        if aws_region in non_supported_regions:
+            print(NONSUPPORTED)
+            send(event, context, SUCCESS, NONSUPPORTED)
+        else:
+            actions.get(req_type)(event)    
+    except Exception as exc:
+        error_msg = {'Error': exc}
+        print(error_msg)
+        send(event, context, FAILED, error_msg)
